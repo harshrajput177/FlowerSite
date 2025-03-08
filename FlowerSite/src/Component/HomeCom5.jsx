afef75effe1ebe15910feddee2a90b9b1f130ba7
harshrajput177 <harshrajput30411@gmail.com>
2025-03-08 20:21:55 +0530

    first  commit

--- FlowerSite/src/Component/HomeCom5.jsx
+++ FlowerSite/src/Component/HomeCom5.jsx
@@ -3,40 +3,40 @@ import '../Style/HomeCom5.css';
 
 const ContactForm = () => {
   return (
-    <div className="contact-container">
-      <h1 className="contact-title">Let's Stay In Touch</h1>
-      <p className="contact-description">
+    <div className="HomeCom5-contact-container">
+      <h1 className="HomeCom5-contact-title">Let's Stay In Touch</h1>
+      <p className="HomeCom5-contact-description">
         Plan a trip for the whole family—from family travel tips to family-friendly filters. We offer 24/7 support on social and through virtual agents onsite.
       </p>
 
-      <div className="contact-form-container">
-        <form className="contact-form">
-          <div  className='contact-form-inputs'>
-            <label className="contact-label">Email</label>
+      <div className="HomeCom5-contact-form-container">
+        <form className="HomeCom5-contact-form">
+          <div  className='HomeCom5-contact-form-inputs'>
+            <label className="HomeCom5-contact-label">Email</label>
             <input
               type="email"
               placeholder="Enter a valid email address"
-              className="contact-input"
+              className="HomeCom5-contact-input"
             />
           </div>
-          <div   className='contact-form-inputs'>
-            <label className="contact-label">Name</label>
+          <div   className='HomeCom5-contact-form-inputs'>
+            <label className="HomeCom5-contact-label">Name</label>
             <input
               type="text"
               placeholder="Enter your Name"
-              className="contact-input"
+              className="HomeCom5-contact-input"
             />
           </div>
-          <div   className='contact-form-inputs'>
-            <label className="contact-label">Message</label>
+          <div   className='HomeCom5-contact-form-inputs'>
+            <label className="HomeCom5-contact-label">Message</label>
             <textarea
               placeholder="Enter your message"
-              className="contact-input"
+              className="HomeCom5-contact-input"
             />
           </div>
           <button
             type="submit"
-            className="contact-submit"
+            className="HomeCom5-contact-submit"
           >
             SUBMIT
           </button>
